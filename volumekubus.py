8dee1f440d52f2d9a6513623e1d7211d5a0c7a1c
dibbjr <80473641+dibbjr@users.noreply.github.com>
2021-03-18 21:48:19 +0700

    Add files via upload

---
+++ volumekubus.py
@@ -0,0 +1,5 @@
+print ("----Program Menghitung Volume Kubus----")
+sisi = float(input(" Masukkan Sisi : "))
+hasil = sisi*sisi*sisi
+print(" Volume Kubus adalah : "+ str(hasil))
+print("----CREATE By:DibbJR----")
